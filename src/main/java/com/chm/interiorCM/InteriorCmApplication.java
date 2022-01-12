fad1952c925ce27e6ae09395a1f8be3ccad3529e
goodmean <zizigoo12@gmail.com>
2022-01-12 15:52:18 +0900

    DB세팅 및 Security Config설정

---
+++ src/main/java/com/chm/interiorCM/InteriorCmApplication.java
@@ -0,0 +1,13 @@
+package com.chm.interiorCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteriorCmApplication.class, args);
+	}
+
+}
