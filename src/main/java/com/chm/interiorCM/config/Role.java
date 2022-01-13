dae4645af1a8449756c37fca2543b1d16787dd46
goodmean <zizigoo12@gmail.com>
2022-01-13 15:15:14 +0900

    Member Entity, Service, Dao 구현

---
+++ src/main/java/com/chm/interiorCM/config/Role.java
@@ -0,0 +1,16 @@
+package com.chm.interiorCM.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public enum Role {
+
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String value;
+
+}
---
+++ src/main/java/com/chm/interiorCM/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.chm.interiorCM.controller;
+
+import com.chm.interiorCM.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+}
