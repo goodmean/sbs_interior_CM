2214877ca8d47cb271fded39d26b1fdb49be5198
goodmean <zizigoo12@gmail.com>
2022-01-14 15:40:09 +0900

    MemberSaveForm @NotBlank

--- src/main/java/com/chm/interiorCM/dto/Member/MemberSaveForm.java
+++ src/main/java/com/chm/interiorCM/dto/Member/MemberSaveForm.java
@@ -2,13 +2,21 @@ package com.chm.interiorCM.dto.Member;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class MemberSaveForm {
     // 회원가입시 필요한 정보
 
+    @NotBlank
     private String loginId;
+    @NotBlank
     private String loginPw;
+    @NotBlank
     private String name;
+    @NotBlank
     private String nickname;
+    @NotBlank
     private String email;
+
 }
