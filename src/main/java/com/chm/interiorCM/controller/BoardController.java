1c09cd60ccea3cb8ae48276357cb34308e048b50
goodmean <zizigoo12@gmail.com>
2022-01-21 14:14:35 +0900

    게시판 list 구현

--- src/main/java/com/chm/interiorCM/controller/BoardController.java
+++ src/main/java/com/chm/interiorCM/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.chm.interiorCM.controller;
 
+import com.chm.interiorCM.domain.Board;
 import com.chm.interiorCM.dto.board.BoardSaveForm;
 import com.chm.interiorCM.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,6 +32,17 @@ public class BoardController {
         boardService.save(boardSaveForm);
 
         return "redirect:/";
+    }
+
+    // list작업
+    @GetMapping("/boards")
+    public String showBoardList(Model model){
+
+        List<Board> boardList = boardService.findAll();
+
+        model.addAttribute("boardList", boardList);
+
+        return "usr/board/list";
     }
 
 }
