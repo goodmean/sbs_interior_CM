3318f8dfa04c9d66e0ecd2f2ab0293fe84e52e4c
goodmean <zizigoo12@gmail.com>
2022-02-04 15:31:14 +0900

    게시물 수정 유효성 검사 구현

--- src/main/java/com/chm/interiorCM/controller/ArticleController.java
+++ src/main/java/com/chm/interiorCM/controller/ArticleController.java
@@ -86,15 +86,21 @@ public class ArticleController {
     }
 
     @PostMapping("/articles/modify/{id}")
-    public String doModify(@PathVariable(name = "id") Long id, ArticleModifyForm articleModifyForm){
+    public String doModify(@PathVariable(name = "id") Long id, ArticleModifyForm articleModifyForm, Principal principal){
 
         try{
+            ArticleDTO findArticle = articleService.getArticle(id);
+
+            if(!findArticle.getMemberLoginId().equals(principal.getName())){
+                throw new IllegalAccessException("잘못된 요청입니다.");
+            }
+
             Board findBoard = boardService.getBoard(id);
 
             articleService.modifyArticle(articleModifyForm, findBoard, id);
             return "redirect:/boards/" + id;
         }catch (Exception e){
-            return "usr/article/modify";
+            return "redirect:/";
         }
 
     }
