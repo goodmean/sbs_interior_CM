d945268a8d665884aba951ee29ea4f5a1bde0260
goodmean <zizigoo12@gmail.com>
2022-02-18 14:42:07 +0900

    syntax error 수정

--- src/main/java/com/chm/interiorCM/config/SecurityConfig.java
+++ src/main/java/com/chm/interiorCM/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests( authorize -> authorize
 
-                .mvcMatchers("/members/join", "/members/login", "members/check/**").anonymous()
+                .mvcMatchers("/members/join", "/members/login", "/members/check/**").anonymous()
                 .mvcMatchers("/articles/**",
                         "/",
                         "/members/modify/**"
