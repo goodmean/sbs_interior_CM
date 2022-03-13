6fa753bb40f519ecad092e2dbad1594ca46ee7bd
goodmean <zizigoo12@gmail.com>
2022-03-13 19:08:49 +0900

    게시판 관리 기능 1차

---
+++ src/main/java/com/chm/interiorCM/dto/adm/AdmBoardCountDto.java
@@ -0,0 +1,11 @@
+package com.chm.interiorCM.dto.adm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdmBoardCountDto {
+}
---
+++ src/main/java/com/chm/interiorCM/dto/adm/BoardStatDto.java
@@ -0,0 +1,14 @@
+package com.chm.interiorCM.dto.adm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardStatDto {
+
+	private Long totalBoardCount;
+
+}
